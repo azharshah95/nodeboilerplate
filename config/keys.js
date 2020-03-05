1acf33712981e6e50f4bc6a690036692a56362d2
Azhar Jamal <azhar shah>
2020-03-05 13:46:55 +0500

    setting up env

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,18 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost:27017/nodeboilerplate',
+  mongoURI: process.env.MONGO_URI,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true
   },
-  secretOrKey: 'secret',
-  jwtExpiry: 900
+  secretOrKey: process.env.SECRET_OR_KEY,
+  jwtExpiry: parseInt(process.env.JWT_EXPIRY),
 }
+// module.exports = {
+//   mongoURI: 'mongodb://localhost:27017/nodeboilerplate',
+//   options: {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   },
+//   secretOrKey: 'secret',
+//   jwtExpiry: 900
+// }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const cors = require('cors');
+const dotenv = require('dotenv').config()
 
 // DB Config
 const db = require('./config/keys');
