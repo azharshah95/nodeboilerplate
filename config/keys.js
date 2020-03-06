7199e24d9b2914a7df72f2f131ff2addfaa57003
Azhar Jamal <azhar shah>
2020-03-06 12:40:20 +0500

    removed unused keys from env and keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,22 +1,9 @@
 module.exports = {
-  // mongoURI: process.env.MONGODB_URI,
-  mongoURI: process.env.MONGOLAB_CRIMSON_URI,
+  mongoURI: process.env.NODE_ENV === 'development' ? process.env.MONGODB_DEV_URI : process.env.MONGODB_PROD_URI,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    user: process.env.USER,
-    pass: process.env.PASS,
-    dbName: process.env.DBNAME
   },
   secretOrKey: process.env.SECRET_OR_KEY,
   jwtExpiry: parseInt(process.env.JWT_EXPIRY),
 }
-// module.exports = {
-//   mongoURI: 'mongodb://localhost:27017/nodeboilerplate',
-//   options: {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   },
-//   secretOrKey: 'secret',
-//   jwtExpiry: 900
-// }
